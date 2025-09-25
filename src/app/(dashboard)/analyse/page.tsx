ba6da579516f506851e1abc10e7a215237c08b46
Casper Nag <casper_nag@outlook.com>
2025-09-25 09:39:44 +0200

    OK

---
+++ src/app/(dashboard)/analyse/page.tsx
@@ -0,0 +1,12 @@
+
+import { PageHeader } from "@/components/shared/PageHeader";
+
+
+export default function AnalysePage() {
+  return (
+    <div className="container mx-auto p-4">
+      <PageHeader title="Analyse"/>
+      <p className="text-xl italic">Denne siden er under konstruksjon</p>
+    </div>
+  );
+}
