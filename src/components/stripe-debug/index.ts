cec3efcaeb7c9e06c12075bbc7e9c8a13c5358c0
Casper Nag <casper_nag@outlook.com>
2026-03-09 13:04:34 +0100

    removed tests

--- src/components/stripe-debug/index.ts
+++
@@ -1,8 +0,0 @@
-// Debug component exports
-export { AdminGuard } from './AdminGuard';
-export { SubscriptionDebugger } from './SubscriptionDebugger';
-export { WebhookDebugger } from './WebhookDebugger';
-export { FirebaseExtensionDebugger } from './FirebaseExtensionDebugger';
-export { StripeConfigDebugger } from './StripeConfigDebugger';
-export { UsageLimitsDebugger } from './UsageLimitsDebugger';
-export { PaymentHistoryDebugger } from './PaymentHistoryDebugger';
