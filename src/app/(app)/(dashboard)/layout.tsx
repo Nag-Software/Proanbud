4134d288e956a35e38d9094cbc7cb906f88df332
canag1045 <casper_nag@outlook.com>
2026-03-17 11:56:57 +0100

    Scrollbar-bug fix

--- src/app/(app)/(dashboard)/layout.tsx
+++ src/app/(app)/(dashboard)/layout.tsx
@@ -16,7 +16,7 @@ export default function DashboardLayout({
       <SubscriptionBlocker>
         <div className="flex flex-col z-1 lg:flex-row bg-background font-sans" data-dashboard>
           <Sidebar />
-          <div className="flex-1 flex flex-col bg-background p-2 overflow-y-auto lg:ml-[280px]">
+          <div className="flex-1 h-full flex flex-col bg-background p-2 overflow-y-auto lg:ml-[280px]">
             <main className="shadow-sm border border-gray-100 bg-white rounded-2xl">
               <TrialHeader />
               <MobileBreadcrumb />
